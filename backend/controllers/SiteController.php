41c72d6f77c224d5e4a4216a959d0b028940aa63
Nikolay <n.poymanov@gmail.com>
2017-05-26 19:52:34 +0300

    Удалены лишние else конструкции

--- backend/controllers/SiteController.php
+++ backend/controllers/SiteController.php
@@ -63,11 +63,11 @@ class SiteController extends Controller
         $model = new LoginForm();
         if ($model->load(Yii::$app->request->post()) && $model->login()) {
             return $this->goBack();
-        } else {
-            return $this->render('login', [
-                'model' => $model,
-            ]);
         }
+
+        return $this->render('login', [
+            'model' => $model,
+        ]);
     }
 
     /**
