adb20564a742112a45eb903e1dd7dc2c6ca8f64f
Nikolay <n.poymanov@gmail.com>
2017-06-04 19:36:52 +0300

    Добавлена заготовка страницы профиля пользователя

--- frontend/config/urlManager.php
+++ frontend/config/urlManager.php
@@ -11,6 +11,13 @@ return [
         '' => 'site/index',
         '<_a:about>' => 'site/<_a>',
         '<_a:login|logout>' => 'auth/auth/<_a>',
+
+        'cabinet' => 'cabinet/default/index',
+        'cabinet/<_c:[\w\-]+>' => 'cabinet/<_c>/index',
+        'cabinet/<_c:[\w\-]+>/<id:\d+>' => 'cabinet/<_c>/view',
+        'cabinet/<_c:[\w\-]+>/<_a:[\w-]+>' => 'cabinet/<_c>/<_a>',
+        'cabinet/<_c:[\w\-]+>/<id:\d+>/<_a:[\w\-]+>' => 'cabinet/<_c>/<_a>',
+
         '<_c:[\w\-]+>' => '<_c>/index',
         '<_c:[\w\-]+>/<id:\d+>' => '<_c>/view',
         '<_c:[\w\-]+>/<_a:[\w-]+>' => '<_c>/<_a>',
