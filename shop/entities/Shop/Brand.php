da3724f4a7517ed52af41c8dc2eef11e15b04489
Nikolay <n.poymanov@gmail.com>
2017-06-06 21:43:06 +0300

    Добавлен класс-поведение для работы с мета-тэгами

--- shop/entities/Shop/Brand.php
+++ shop/entities/Shop/Brand.php
@@ -4,7 +4,7 @@ namespace shop\entities\Shop;
 
 use shop\entities\Meta;
 use yii\db\ActiveRecord;
-use yii\helpers\Json;
+use shop\entities\behaviors\MetaBehavior;
 
 /**
  * @property integer $id
@@ -39,20 +39,10 @@ class Brand extends ActiveRecord
         return '{{%shop_brands}}';
     }
 
-    public function afterFind(): void
+    public function behaviors()
     {
-        $meta = Json::decode($this->getAttribute('meta_json'));
-        $this->meta = new Meta($meta['title'], $meta['description'], $meta['keywords']);
-        parent::afterFind();
-    }
-
-    public function beforeSave($insert): bool
-    {
-        $this->setAttribute('meta_json', Json::encode([
-            'title' => $this->meta->title,
-            'description' => $this->meta->description,
-            'keywords' => $this->meta->keywords,
-        ]));
-        return parent::beforeSave($insert);
+        return [
+            MetaBehavior::className(),
+        ];
     }
 }
